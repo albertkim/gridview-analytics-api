73536d183651917d810379edc468828af973c8f0
lisqr <li.mw.lisa@gmail.com>
2023-11-28 22:59:39 -0800

    Add typings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express from 'express'
+import { express, Request, Response } from 'express'
 import knex from './database'
 import 'dotenv/config'
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/ping', async (req, res) => {
+app.get('/ping', async (req: Request, res: Response) => {
   res.send({
     data: 'Hello world',
     date: new Date()
   })
 })
 
-app.get('/api/v1/news', async (req, res) => {
+app.get('/api/v1/news', async (req: Request, res: Response) => {
   const news = await knex('news')
   res.send({
     data: news
