31da1a6290565e8b9e646b61e16c88466a614ddd
Albert Kim <albertkim@Alberts-MacBook-Pro.local>
2024-03-11 15:19:12 -0700

    Fine-tuned the article summarization query

--- src/services/analyze-raw-news/index.ts
+++ src/services/analyze-raw-news/index.ts
@@ -36,7 +36,17 @@ export async function analyzeRawNews(rawNews: IRawNews): Promise<IAnalyzedNews> 
 
   try {
     newContents = await chatGPTTextQuery(`
-      You are a news article summarizing the provided document, crafted for mostly city policy and real estate professionals. Replace the original document by providing an immediate and direct overview of the key points, without referring to yourself as a separate entity. Include specifics and practical details that real estate agents would find useful. Begin with a succinct 2-3 sentence introduction, followed by bullet points for detailed information. Keep the number of bullet points between 3-5 if possible. Clearly indicate the current stage of any legislative item, including relevant dates. No yapping, exclude fluffyu language, and be specific with items, dates, dollar values, address, names, etc.
+      You are a news article summarizing the provided document, crafted for mostly city policy and real estate professionals. Replace the original document by providing an immediate and direct overview of the key points, without referring to yourself as a separate entity. Below is an example of the format I would like to see in the response, but you can use your own words and structure as long as it is clear and concise. Here is the example format:
+
+      <p>2-3 sentence summary</p>
+      <ul>
+        <li><b>Bullet point header </b>Bullet point contents</li>
+        <li><b>Bullet point header </b>Bullet point contents</li>
+      </ul>
+      
+      Do not include a title. Keep the number of bullet points between 3-5 if possible. You can have nested bullet points if useful. Clearly indicate the current stage of any legislative item, including relevant dates. No yapping, exclude fluffy language, and be specific with items, dates, dollar values, address, names, etc.
+      
+      Return in HTML format using only p ul li and b tags. Only bold list items, do not bold anything at the start.
 
       Here are the contents:
 
@@ -46,6 +56,7 @@ export async function analyzeRawNews(rawNews: IRawNews): Promise<IAnalyzedNews> 
       throw createHttpError(500, `Error getting contents`)
     }
   } catch (error) {
+    console.error(error)
     throw createHttpError(500, `Error parsing PDF for report contents`)
   }
 
@@ -59,6 +70,7 @@ export async function analyzeRawNews(rawNews: IRawNews): Promise<IAnalyzedNews> 
       throw createHttpError(500, `Error getting title`)
     }
   } catch (error) {
+    console.error(error)
     throw createHttpError(500, `Error parsing PDF for report contents`)
   }
 
