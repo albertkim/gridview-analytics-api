0585ba1b643451fcb52a0d106846674f442eec7b
Albert Kim <albert275@gmail.com>
2024-03-17 15:39:59 -0700

    Cleaned up old unused ai code, added anthropic api key

--- src/utilities/AIUtilities.ts
+++ src/utilities/AIUtilities.ts
@@ -1,18 +1,29 @@
 import dotenv from 'dotenv'
 import OpenAI from 'openai'
+import Anthropic from '@anthropic-ai/sdk'
 import chalk from 'chalk'
 import { ImageAnnotatorClient } from '@google-cloud/vision'
 import createHttpError from 'http-errors'
 
 dotenv.config()
 
-const chatGPTAPIKey = process.env.CHAT_GPT_API_KEY!
+const chatGPTAPIKey = process.env.CHAT_GPT_API_KEY
 
 let openai: OpenAI | undefined
 
 if (chatGPTAPIKey) {
 	openai = new OpenAI({
 		apiKey: chatGPTAPIKey
+	})
+}
+
+const anthropicAPIKey = process.env.ANTHROPIC_API_KEY
+
+let anthropic: Anthropic
+
+if (anthropicAPIKey) {
+	anthropic = new Anthropic({
+		apiKey: anthropicAPIKey
 	})
 }
 
@@ -165,77 +176,5 @@ export async function imageTextQuery(query: string, fileData: string, gptVersion
 			throw new Error()
 		}
 	}
-
-}
-
-export function getGPTBaseRezoningQuery(document: string, options?: BaseRezoningQueryParams) {
-
-  return `
-		Carefully read the provided document and give me the following in a JSON format - otherwise return a {error: message, reason: detailed explanation}.
-		${options?.introduction ? options.introduction : ''}
-    {
-      applicationId: ${options?.applicationId ? options.applicationId : 'the unique alphanumeric identifier for this rezoning, always a string, null if not specified'} 
-      address: address in question - only street address, no city - if multiple addresses, comma separate, null if doesn't exist
-      applicant: who the rezoning applicant is
-      behalf: if the applicant is applying on behalf of someone else, who is it - null if doesn't exist
-      description: a description of the rezoning and what the applicant wants to build - be specific, include numerical metrics
-      buildingType: one of single-family residential (including duplexes), townhouse, mixed use (only if there is residential + commercial), multi-family residential (only if there is no commercial), industrial (manufacturing, utilities, etc.), commercial, or other
-      status: ${options?.status ? options.status : 'one of applied, public hearing, approved, denied, withdrawn'} 
-      stats: {
-        buildings: your best guess as to the number of new buildings being proposed or null if unclear
-        stratas: your best guess as to the total number of non-rental residential units/townhouses or null if unclear - default to assuming non-rental units
-        rentals: total number of rental units or null if unclear - do not default to rental if not specified
-        hotels: total number of hotel units (not buildings) or null if unclear
-        fsr: total floor space ratio or null if unclear
-				storeys: total number of storeys - pick the tallest if multiple - null if unclear
-      }
-      zoning: {
-        previousZoningCode: city zoning code before rezoning or null if unclear - keep short
-        previousZoningDescription: best description of previous zoning code (ex. low density residential)
-        newZoningCode: city zoning code after rezoning or null if unclear - keep short
-        newZoningDescription: best description of new zoning code (ex. high density residential)
-      }
-    }
-    Document here: ${document}
-  `
-
-}
-
-export function getGPTBaseRezoningTypeQuery(description: string) {
-	
-	return `
-		Here are some descriptions of possible rezoning types:
-		- single-family residential (include duplexes)
-		- townhouse
-		- mixed use (only if there is residential + commercial, also usually includes comprehensive developments)
-		- multi-family residential
-		- industrial (manufacturing, utilities, etc.)
-		- commercial (offices, sales, hotels, etc.)
-		- other (only if absolutely nothing else fits)
-
-		Given the following description, give me the following in a JSON format. 
-		{
-			buildingType: one of single-family residential, townhouse, mixed use, multi-family residential, industrial, commercial, or other
-		}
-
-		Description here: ${description}
-	`
-
-}
-
-export function getGPTBaseRezoningStatsQuery(description: string) {
-
-  return `
-    Given the following description, give me the following in a JSON format - always return all fields even if null:
-    {
-      buildings: your best guess as to the number of new buildings being proposed - default to 1 - null if unclear - if it's a townhouse, default to 1 unless it's clear that there are multiple separated structures
-      stratas: your best guess as to the total number of non-rental residential units/townhouses or null if unclear - default to assuming non-rental units - 0 if it's a commercial/industrial development
-      rentals: total number of rental units - 0 if no explicit mention of rentals - null if unclear
-      hotels: total number of hotel units - 0 if no explicit mention of hotels - null if unclear
-      fsr: total floor space ratio or null if unclear
-			storeys: total number of storeys - pick the tallest if multiple - null if unclear
-    }
-    Description here: ${description}
-  `
 
 }
