378c45d22e9fede9367eec32eeafed02b3c3b557
Albert Kim <albert275@gmail.com>
2024-03-27 12:37:44 -0700

    Added a backfill news tag script, not run yet

---
+++ src/scripts/fill-news-tags.ts
@@ -0,0 +1,39 @@
+import 'module-alias/register'
+import { NewsRepository } from '@/repositories/NewsRepository'
+import { chatGPTJSONQuery } from '@/utilities/AIUtilities'
+
+(async () => {
+
+  const news = await NewsRepository.getNews({offset: 0, limit: null, city: null, important: null})
+
+  console.log(news.data.length)
+
+  for (const newsItem of news.data) {
+
+    const tagsResult = await chatGPTJSONQuery(`
+      <Document>
+        ${newsItem.summary}
+      </Document>
+
+      Given the document above, return in the following JSON format:
+
+      {
+        "tags": ["tag1", "tag2"]
+      }
+
+      Tags: An array of tags, can be one of "transportation" (highways, roads, public transit), "development" (rezonings, development permits, construction of new structures), "finance" (budgets, etc.), "services" (police, fire, etc.), "community" (parks, events, etc., don't include with development), "infrastructure" (public utilities, not transportation-related), or "other" if none of the above. Try to only have 1 tag.
+    `)
+
+    if (tagsResult.tags.length === 0) {
+      console.error(`No tags found for ${newsItem.id}`)
+      continue
+    }
+
+    console.log(`Updating news "${newsItem.title}" with tags ${tagsResult.tags}`)
+    newsItem.tags = tagsResult.tags
+
+    await NewsRepository.updateNews(newsItem.id, newsItem)
+
+  }
+
+})()
